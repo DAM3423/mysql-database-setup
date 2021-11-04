99ed3a80d450800cf0814c0bceb175ed945eef41
Dominic Marshall <42091080+DAM3423@users.noreply.github.com>
2021-11-04 21:06:14 +0000

    Create setup-mysql-database.sh

---
+++ setup-mysql-database.sh
@@ -0,0 +1,16 @@
+# CREATE MYSQL DATABASE
+
+echo "We need to set up a Database for you."
+read mysql_database
+
+echo "We now need to set up a MYSQL user for you."
+read mysql_user
+
+echo "Great! Finally, We need a password for this very lucky user."
+read mysql_password
+
+sudo mysql <<EOF
+CREATE DATABASE IF NOT EXISTS ${mysql_database};
+CREATE USER '${mysql_user}'@'%' IDENTIFIED WITH mysql_native_password BY '${mysql_password}';
+GRANT ALL ON ${mysql_database}.* TO '${mysql_user}'@'%';
+EOF
